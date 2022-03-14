e9cc8d321fbaa61fa5677b4782e8da04d14ebe9c
Fadhil706 <93008556+Fadhil706@users.noreply.github.com>
2022-03-14 10:20:22 +0700

    Add files via upload

---
+++ TUGAS PRAKTIKUM/bawah.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <footer>
+            <p>Created by <a href="http://nurulfikri.ac.id">Pusinfo NF &copy;2017</a></p>
+    </footer>
+    
+</body>
+</html>
+
+<?php
+
+    
+
+?>
